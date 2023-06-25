a5922ea7e13bb66682e97ced82cfb1fa2aba8d01
Somya Sarathi Samal <soms9edu@gmail.com>
2023-06-25 15:31:33 +0530

    Solved binary tree problem:     1. invert_binary_tree

---
+++ binary_tree/invert_binary_tree.py
@@ -0,0 +1,16 @@
+import unittest
+from TreeNode import TreeNode
+from typing import Optional
+
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
